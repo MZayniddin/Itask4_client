56488146115f6ce41ee20563d8ffbc9103c6d45c
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-08-16 08:41:32 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentUser } from "./store/auth/auth.selector";
+import jwtDecode from "jwt-decode";
+
+import Auth from "./pages/Auth";
+import Users from "./pages/Users";
+
+const token = localStorage.getItem("token");
+
+interface MyToken {
+  id: number;
+  email: string;
+  displayname: string;
+  status: boolean;
+  iat: number;
+  exp: number;
+}
+
+const App = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectCurrentUser);
+
+  useEffect(() => {
+    const decodedToken: MyToken =
+      jwtDecode(localStorage.getItem("token")) || null;
+
+    if (decodedToken) {
+      localStorage.setItem("user", decodedToken.displayname);
+      if (decodedToken?.exp * 1000 < new Date().getTime()) {
+        dispatch({ type: "LOGOUT" });
+      }
+    }
+  }, []);
+
+  return (
+    <Routes>
+      <Route
+        index
+        element={token || user ? <Users /> : <Navigate to="/auth" />}
+      />
+      <Route path="auth" element={<Auth />} />
+    </Routes>
+  );
+};
+
+export default App;
